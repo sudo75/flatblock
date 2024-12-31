1046d2aea05ab19a174e08935a45783a85d1beb8
Eric Skaftason <skaftasoneric@gmail.com>
2024-12-31 17:08:06 -0500

    added input system to log multiple keys

--- src/inputs/input.js
+++ src/inputs/input.js
@@ -2,7 +2,28 @@ class InputHandler {
     constructor() {
         this.keys = []; //arr to store held keys
 
-        //Add event listeners here
+        this.acceptedKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+
+        window.addEventListener('keydown', (event) => {
+            const key = event.key;
+            if (this.keys.indexOf(key) !== -1) return;
+
+            if (this.acceptedKeys.includes(key)) {
+                this.keys.push(key);
+            }
+            console.log(this.keys)
+        });
+
+        window.addEventListener('keyup', (event) => {
+            const key = event.key;
+            if (this.keys.indexOf(key) === -1) return;
+
+            if (this.keys.includes(key)) {
+                this.keys.splice(this.keys.indexOf(key), 1);
+            }
+
+            console.log(this.keys)
+        });
     }
 
     
