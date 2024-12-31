75f1008af53350c6e6e57b02156d7af32345016f
Eric Skaftason <skaftasoneric@gmail.com>
2024-12-30 22:55:55 -0500

    added new classes for different block types

---
+++ src/generation/blocks.js
@@ -0,0 +1,43 @@
+class Block {
+    constructor(name, x, y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Block_Solid extends Block {
+    constructor(name, x, y) {
+        super(name, x, y);
+        this.type = 'solid';
+        this.viscosity = 1;
+        this.transparency = 0;
+    }
+}
+
+class Block_Liquid extends Block {
+    constructor(name, x, y) {
+        super(name, x, y);
+        this.type = 'liquid';
+        this.viscosity = 0;
+        this.transparency = 0.8;
+    }
+}
+
+class Block_Air extends Block {
+    constructor(x, y) {
+        super('air', x, y);
+        this.type = 'air';
+        this.viscosity = 0;
+        this.transparency = 1;
+    }
+}
+
+class Block_dirt extends Block_Solid {
+    constructor(x, y) {
+        super('dirt', x, y);
+    }
+
+}
+
+export { Block_Air, Block_dirt };
