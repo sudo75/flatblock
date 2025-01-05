6ce7f31d77cf943a12f03600e68efb2dd82cba78
Eric Skaftason <skaftasoneric@gmail.com>
2025-01-05 14:48:21 -0500

    added block placing with right click

--- src/generation/level.js
+++ src/generation/level.js
@@ -68,8 +68,63 @@ class Level {
     }
 
     world_interaction() {
+        this.computeBlockBreaking();
+        this.computeBlockPlacing();
+    }
+
+    placeBlock(blockID, x, y) {
+        let block;
+        switch (blockID) { //UPGRADE CODE LATER
+            case 1:
+                block = Block_dirt;
+                break;
+            default:
+                console.warn('Can\'t place block!');
+                return;
+        }
+
+        this.data[this.calc.getChunkID(x)].block_data[this.calc.getRelativeX(x)][y] = new block(x, y);
+    }
+
+    breakBlock(x, y) {
+        this.data[this.calc.getChunkID(x)].block_data[this.calc.getRelativeX(x)][y] = new Block_Air(x, y);
+    }
+
+    blockCanBePlaced(x, y) {
+        const playerX_min = Math.floor(this.game.player.x);
+        const playerX_max = this.calc.hardRoundDown(this.game.player.x + this.game.player.width_blocks);
+        const playerY_min = Math.floor(this.game.player.y);
+        const playerY_max = this.calc.hardRoundDown(this.game.player.y + this.game.player.height_blocks);
+
+        for (let i = playerX_min; i <= playerX_max; i++) {
+            for (let j = playerY_min; j <= playerY_max; j++) {
+                if (x === i && y === j) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    computeBlockPlacing() {
+        if (this.game.player.selectedBlock.x == null || this.game.player.selectedBlock.y == null) return;
+
+        if (this.game.input.mouseDown_right) {
+            if (
+                this.calc.getBlockData(this.game.player.selectedBlock.x, this.game.player.selectedBlock.y).type !== 'solid' &&
+                this.game.player.getBlockDistance(this.game.player.selectedBlock.x, this.game.player.selectedBlock.y) <= this.game.player.cursorDistLim &&
+                this.calc.solidBlockAdjacent(this.game.player.selectedBlock.x, this.game.player.selectedBlock.y) &&
+                this.blockCanBePlaced(this.game.player.selectedBlock.x, this.game.player.selectedBlock.y)
+            ) {
+                this.placeBlock(this.game.player.inventory.data[this.game.player.inventory.selection_index].id, this.game.player.selectedBlock.x, this.game.player.selectedBlock.y);
+            }
+        }
+    }
+
+    computeBlockBreaking() {
         const resetBreakStatus = () => {
-            if (this.current_breaking) {
+            if (this.current_breaking && this.calc.getBlockData(this.current_breaking.x, this.current_breaking.y).hardness) {
                 this.data[this.calc.getChunkID(this.current_breaking.x)].block_data[this.calc.getRelativeX(this.current_breaking.x)][this.current_breaking.y].break_status = 0;
             }
             this.current_breaking = null;
@@ -80,21 +135,23 @@ class Level {
                     resetBreakStatus();
                 }
             }
-            //console.log(this.calc.getBlockDistance(this.current_breaking.x, this.current_breaking.y, this.game.player.x + this.game.player.width_blocks / 2, this.game.player.y + this.game.player.height_blocks / 2), this.game.player.cursorDistLim)
 
             this.current_breaking = {
                 x: this.game.player.selectedBlock.x,
                 y: this.game.player.selectedBlock.y
             };
 
-            if (this.calc.getBlockDistance(this.current_breaking.x, this.current_breaking.y, this.game.player.x + this.game.player.width_blocks / 2, this.game.player.y + this.game.player.height_blocks / 2) <= this.game.player.cursorDistLim) {
+            if (this.game.player.getBlockDistance(this.current_breaking.x, this.current_breaking.y) <= this.game.player.cursorDistLim) {
     
                 //Set break status
-                this.data[this.calc.getChunkID(this.current_breaking.x)].block_data[this.calc.getRelativeX(this.current_breaking.x)][this.current_breaking.y].break_status++;
-    
+                if (this.calc.getBlockData(this.current_breaking.x, this.current_breaking.y).type === 'solid' && this.calc.getBlockData(this.current_breaking.x, this.current_breaking.y).hardness) {
+                    this.data[this.calc.getChunkID(this.current_breaking.x)].block_data[this.calc.getRelativeX(this.current_breaking.x)][this.current_breaking.y].break_status++;
+                }
+
                 //Break block
                 if (this.data[this.calc.getChunkID(this.current_breaking.x)].block_data[this.calc.getRelativeX(this.current_breaking.x)][this.current_breaking.y].break_status >= this.data[this.calc.getChunkID(this.current_breaking.x)].block_data[this.calc.getRelativeX(this.current_breaking.x)][this.current_breaking.y].hardness) {
-                    this.data[this.calc.getChunkID(this.current_breaking.x)].block_data[this.calc.getRelativeX(this.current_breaking.x)][this.current_breaking.y] = new Block_Air(this.current_breaking.x, this.current_breaking.y);
+                    //this.data[this.calc.getChunkID(this.current_breaking.x)].block_data[this.calc.getRelativeX(this.current_breaking.x)][this.current_breaking.y] = new Block_Air(this.current_breaking.x, this.current_breaking.y);
+                    this.breakBlock(this.current_breaking.x, this.current_breaking.y);
                     this.current_breaking = null;
                 }
             }
