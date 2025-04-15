60f701942f9c83737045ff5d571025f74975d391
Eric Skaftason <skaftasoneric@gmail.com>
2025-04-14 22:16:40 -0400

    added new tools (wood, stone, copper, gold, iron, diamond) (pickaxe, axe, shovel, sword)

--- generation/blocks.js
+++ generation/blocks.js
@@ -90,57 +90,324 @@ class Item_stick extends Item {
     }
 }
 
-class Item_woodenPickaxe extends Tool {
+// Pickaxes ---------------------------->
+
+class Item_pickaxe extends Tool {
+    constructor(name, texture_location) {
+        super(name, texture_location);
+        this.purpose = [3, 24, 26, 28, 30, 32]; //list of block IDs the tool breaks
+
+    }
+}
+
+class Item_woodenPickaxe extends Item_pickaxe {
     constructor() {
         super('wooden_pickaxe', './assets/items/wooden_pickaxe.png');
         this.id = 129;
         this.durability = 32;
 
         this.strength = 2;
-        this.damage = 2;
-        this.purpose = [3, 24]; //list of block IDs the tool breaks
+        this.damage = 3;
 
         this.fuel_value = 200;
     }
 }
 
-class Item_stonePickaxe extends Tool {
+class Item_stonePickaxe extends Item_pickaxe {
     constructor() {
         super('stone_pickaxe', './assets/items/stone_pickaxe.png');
         this.id = 130;
         this.durability = 64;
 
-        this.strength = 4;
-        this.damage = 3;
-        this.purpose = [3, 24]; //list of block IDs the tool breaks
+        this.strength = 3;
+        this.damage = 4;
     }
 }
 
-class Item_woodenAxe extends Tool {
+class Item_copperPickaxe extends Item_pickaxe {
     constructor() {
-        super('wooden_axe', './assets/items/wooden_axe.png');
+        super('copper_pickaxe', './assets/items/copper_pickaxe.png');
         this.id = 131;
-        this.durability = 32;
+        this.durability = 192;
 
-        this.strength = 2;
-        this.damage = 3;
-        this.purpose = [4, 5, 7, 8]; //list of block IDs the tool breaks
+        this.strength = 5;
+        this.damage = 4;
 
         this.fuel_value = 200;
     }
 }
 
-class Item_stoneAxe extends Tool {
+class Item_goldenPickaxe extends Item_pickaxe {
     constructor() {
-        super('stone_axe', './assets/items/stone_axe.png');
+        super('golden_pickaxe', './assets/items/golden_pickaxe.png');
         this.id = 132;
-        this.durability = 64;
+        this.durability = 96;
 
-        this.strength = 4;
+        this.strength = 7;
         this.damage = 4;
-        this.purpose = [4, 5, 7, 8]; //list of block IDs the tool breaks
+
+        this.fuel_value = 200;
     }
 }
+
+class Item_ironPickaxe extends Item_pickaxe {
+    constructor() {
+        super('iron_pickaxe', './assets/items/iron_pickaxe.png');
+        this.id = 133;
+        this.durability = 256;
+
+        this.strength = 6;
+        this.damage = 5;
+    }
+}
+
+
+class Item_diamondPickaxe extends Item_pickaxe {
+    constructor() {
+        super('diamond_pickaxe', './assets/items/diamond_pickaxe.png');
+        this.id = 134;
+        this.durability = 1024;
+
+        this.strength = 8;
+        this.damage = 6;
+    }
+}
+
+
+// Axes ---------------------------->
+
+class Item_axe extends Tool {
+    constructor(name, texture_location) {
+        super(name, texture_location);
+        this.purpose = [4, 5, 7, 8]; //list of block IDs the tool breaks
+
+    }
+}
+
+class Item_woodenAxe extends Item_axe {
+    constructor() {
+        super('wooden_axe', './assets/items/wooden_axe.png');
+        this.id = 135;
+        this.durability = 32;
+
+        this.strength = 2;
+        this.damage = 3;
+
+        this.fuel_value = 200;
+    }
+}
+
+class Item_stoneAxe extends Item_axe {
+    constructor() {
+        super('stone_axe', './assets/items/stone_axe.png');
+        this.id = 136;
+        this.durability = 64;
+
+        this.strength = 3;
+        this.damage = 4;
+    }
+}
+
+class Item_copperAxe extends Item_axe {
+    constructor() {
+        super('copper_axe', './assets/items/copper_axe.png');
+        this.id = 137;
+        this.durability = 192;
+
+        this.strength = 5;
+        this.damage = 5;
+    }
+}
+
+class Item_goldenAxe extends Item_axe {
+    constructor() {
+        super('golden_axe', './assets/items/golden_axe.png');
+        this.id = 138;
+        this.durability = 96;
+
+        this.strength = 7;
+        this.damage = 5;
+    }
+}
+
+class Item_ironAxe extends Item_axe {
+    constructor() {
+        super('iron_axe', './assets/items/iron_axe.png');
+        this.id = 139;
+        this.durability = 256;
+
+        this.strength = 6;
+        this.damage = 6;
+    }
+}
+
+class Item_diamondAxe extends Item_axe {
+    constructor() {
+        super('diamond_axe', './assets/items/diamond_axe.png');
+        this.id = 140;
+        this.durability = 1024;
+
+        this.strength = 8;
+        this.damage = 7;
+    }
+}
+
+// Shovels ---------------------------->
+
+class Item_shovel extends Tool {
+    constructor(name, texture_location) {
+        super(name, texture_location);
+        this.purpose = [1, 2]; //list of block IDs the tool breaks
+
+    }
+}
+
+class Item_woodenShovel extends Item_shovel {
+    constructor() {
+        super('wooden_shovel', './assets/items/wooden_shovel.png');
+        this.id = 141;
+        this.durability = 32;
+
+        this.strength = 2;
+        this.damage = 2;
+
+        this.fuel_value = 200;
+    }
+}
+
+class Item_stoneShovel extends Item_shovel {
+    constructor() {
+        super('stone_shovel', './assets/items/stone_shovel.png');
+        this.id = 142;
+        this.durability = 64;
+
+        this.strength = 3;
+        this.damage = 3;
+    }
+}
+
+class Item_copperShovel extends Item_shovel {
+    constructor() {
+        super('copper_shovel', './assets/items/copper_shovel.png');
+        this.id = 143;
+        this.durability = 192;
+
+        this.strength = 5;
+        this.damage = 3;
+    }
+}
+
+class Item_goldenShovel extends Item_shovel {
+    constructor() {
+        super('golden_shovel', './assets/items/golden_shovel.png');
+        this.id = 144;
+        this.durability = 96;
+
+        this.strength = 7;
+        this.damage = 3;
+    }
+}
+
+class Item_ironShovel extends Item_shovel {
+    constructor() {
+        super('iron_shovel', './assets/items/iron_shovel.png');
+        this.id = 145;
+        this.durability = 256;
+
+        this.strength = 6;
+        this.damage = 4;
+    }
+}
+
+class Item_diamondShovel extends Item_shovel {
+    constructor() {
+        super('diamond_shovel', './assets/items/diamond_shovel.png');
+        this.id = 146;
+        this.durability = 1024;
+
+        this.strength = 8;
+        this.damage = 5;
+    }
+}
+
+// Sword ---------------------------->
+
+class Item_sword extends Tool {
+    constructor(name, texture_location) {
+        super(name, texture_location);
+        this.purpose = [6, 7]; //list of block IDs the tool breaks
+
+    }
+}
+
+class Item_woodenSword extends Item_sword {
+    constructor() {
+        super('wooden_sword', './assets/items/wooden_sword.png');
+        this.id = 147;
+        this.durability = 32;
+
+        this.strength = 2;
+        this.damage = 4;
+
+        this.fuel_value = 200;
+    }
+}
+
+class Item_stoneSword extends Item_sword {
+    constructor() {
+        super('stone_sword', './assets/items/stone_sword.png');
+        this.id = 148;
+        this.durability = 64;
+
+        this.strength = 3;
+        this.damage = 5;
+    }
+}
+
+class Item_copperSword extends Item_sword {
+    constructor() {
+        super('copper_sword', './assets/items/copper_sword.png');
+        this.id = 149;
+        this.durability = 192;
+
+        this.strength = 5;
+        this.damage = 6;
+    }
+}
+
+class Item_goldenSword extends Item_sword {
+    constructor() {
+        super('golden_sword', './assets/items/golden_sword.png');
+        this.id = 150;
+        this.durability = 96;
+
+        this.strength = 7;
+        this.damage = 6;
+    }
+}
+
+class Item_ironSword extends Item_sword {
+    constructor() {
+        super('iron_sword', './assets/items/iron_sword.png');
+        this.id = 151;
+        this.durability = 256;
+
+        this.strength = 6;
+        this.damage = 7;
+    }
+}
+
+class Item_diamondSword extends Item_sword {
+    constructor() {
+        super('diamond_sword', './assets/items/diamond_sword.png');
+        this.id = 152;
+        this.durability = 1024;
+
+        this.strength = 8;
+        this.damage = 8;
+    }
+}
+
 
 // BLOCKS ----------------------------------------->
 
@@ -247,9 +514,9 @@ class Item_coal extends Item {
     }
 }
 
-class Block_ironOre extends Block_Solid {
+class Block_copperOre extends Block_Solid {
     constructor(x, y) {
-        super('iron_ore', x, y, 140, './assets/textures/iron_ore.png');
+        super('copper_ore', x, y, 140, './assets/textures/copper_ore.png');
         this.id = 26;
         this.itemDrop_id = 26;
 
@@ -257,16 +524,16 @@ class Block_ironOre extends Block_Solid {
     }
 }
 
-class Item_ironIngot extends Item {
+class Item_copperIngot extends Item {
     constructor() {
-        super('iron_ingot', './assets/items/iron_ingot.png');
+        super('copper_ingot', './assets/items/copper_ingot.png');
         this.id = 27;
     }
 }
 
-class Block_copperOre extends Block_Solid {
+class Block_goldOre extends Block_Solid {
     constructor(x, y) {
-        super('copper_ore', x, y, 140, './assets/textures/copper_ore.png');
+        super('gold_ore', x, y, 140, './assets/textures/gold_ore.png');
         this.id = 28;
         this.itemDrop_id = 28;
 
@@ -274,16 +541,16 @@ class Block_copperOre extends Block_Solid {
     }
 }
 
-class Item_copperIngot extends Item {
+class Item_goldIngot extends Item {
     constructor() {
-        super('copper_ingot', './assets/items/copper_ingot.png');
+        super('gold_ingot', './assets/items/gold_ingot.png');
         this.id = 29;
     }
 }
 
-class Block_goldOre extends Block_Solid {
+class Block_ironOre extends Block_Solid {
     constructor(x, y) {
-        super('gold_ore', x, y, 140, './assets/textures/gold_ore.png');
+        super('iron_ore', x, y, 140, './assets/textures/iron_ore.png');
         this.id = 30;
         this.itemDrop_id = 30;
 
@@ -291,9 +558,9 @@ class Block_goldOre extends Block_Solid {
     }
 }
 
-class Item_goldIngot extends Item {
+class Item_ironIngot extends Item {
     constructor() {
-        super('gold_ingot', './assets/items/gold_ingot.png');
+        super('iron_ingot', './assets/items/iron_ingot.png');
         this.id = 31;
     }
 }
@@ -466,20 +733,44 @@ class Item_Directory {
 
             '24': Block_coalOre,
             '25': Item_coal,
-            '26': Block_ironOre,
-            '27': Item_ironIngot,
-            '28': Block_copperOre,
-            '29': Item_copperIngot,
-            '30': Block_goldOre,
-            '31': Item_goldIngot,
+            '26': Block_copperOre,
+            '27': Item_copperIngot,
+            '28': Block_goldOre,
+            '29': Item_goldIngot,
+            '30': Block_ironOre,
+            '31': Item_ironIngot,
             '32': Block_diamondOre,
             '33': Item_diamond,
 
             '128': Item_stick,
+
             '129': Item_woodenPickaxe,
             '130': Item_stonePickaxe,
-            '131': Item_woodenAxe,
-            '132': Item_stoneAxe,
+            '131': Item_copperPickaxe,
+            '132': Item_goldenPickaxe,
+            '133': Item_ironPickaxe,
+            '134': Item_diamondPickaxe,
+
+            '135': Item_woodenAxe,
+            '136': Item_stoneAxe,
+            '137': Item_copperAxe,
+            '138': Item_goldenAxe,
+            '139': Item_ironAxe,
+            '140': Item_diamondAxe,
+
+            '141': Item_woodenShovel,
+            '142': Item_stoneShovel,
+            '143': Item_copperShovel,
+            '144': Item_goldenShovel,
+            '145': Item_ironShovel,
+            '146': Item_diamondShovel,
+
+            '147': Item_woodenSword,
+            '148': Item_stoneSword,
+            '149': Item_copperSword,
+            '150': Item_goldenSword,
+            '151': Item_ironSword,
+            '152': Item_diamondSword
         }
     }
 
--- menus/crafting_recipies.js
+++ menus/crafting_recipies.js
@@ -96,7 +96,7 @@ const recipies = [
         ]
     },
 
-    { //2 planks => 4 sticks
+    { // 2 planks => 4 sticks
         result: 128,
         quantity: 4,
         recipie: [
@@ -133,32 +133,8 @@ const recipies = [
         ]
     },
 
-    { //3 planks + 2 sticks => 1 wooden picaxe
-        result: 129,
-        quantity: 1,
-        recipie: [
-            [
-                [8, 8, 8],
-                [null, 128, null],
-                [null, 128, null]
-            ]
-        ]
-    },
-
-    { //3 stone + 2 sticks => 1 stone picaxe
-        result: 130,
-        quantity: 1,
-        recipie: [
-            [
-                [3, 3, 3],
-                [null, 128, null],
-                [null, 128, null]
-            ]
-        ]
-    },
-
-    { //3 planks + 2 sticks => 1 wooden axe
-        result: 131,
+    { // 3 planks + 2 sticks => 1 wooden axe
+        result: 135,
         quantity: 1,
         recipie: [
             [
@@ -174,8 +150,8 @@ const recipies = [
         ]
     },
 
-    { //3 stone + 2 sticks => 1 stone axe
-        result: 132,
+    { // 3 stone + 2 sticks => 1 stone axe
+        result: 136,
         quantity: 1,
         recipie: [
             [
@@ -191,7 +167,7 @@ const recipies = [
         ]
     },
 
-    { //8 planks => 1 chest
+    { // 8 planks => 1 chest
         result: 9,
         quantity: 1,
         recipie: [
@@ -203,7 +179,7 @@ const recipies = [
         ]
     },
 
-    { //4 planks => 1 crafting table
+    { // 4 planks => 1 crafting table
         result: 10,
         quantity: 1,
         recipie: [
@@ -228,7 +204,454 @@ const recipies = [
                 [null, 8, 8]
             ]
         ]
-    }
+    },
+
+
+    // PICKAXES --------------------------->
+
+    { // 3 planks + 2 sticks => 1 wooden pickaxe
+        result: 129,
+        quantity: 1,
+        recipie: [
+            [
+                [8, 8, 8],
+                [null, 128, null],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    { // 3 stone + 2 sticks => 1 stone pickaxe
+        result: 130,
+        quantity: 1,
+        recipie: [
+            [
+                [3, 3, 3],
+                [null, 128, null],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    { // 3 copper + 2 copper => 1 copper pickaxe
+        result: 131,
+        quantity: 1,
+        recipie: [
+            [
+                [27, 27, 27],
+                [null, 128, null],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    { // 3 gold + 2 sticks => 1 golden pickaxe
+        result: 132,
+        quantity: 1,
+        recipie: [
+            [
+                [29, 29, 29],
+                [null, 128, null],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    { // 3 iron + 2 sticks => 1 iron pickaxe
+        result: 133,
+        quantity: 1,
+        recipie: [
+            [
+                [31, 31, 31],
+                [null, 128, null],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    { // 3 diamonds + 2 sticks => 1 diamond pickaxe
+        result: 134,
+        quantity: 1,
+        recipie: [
+            [
+                [33, 33, 33],
+                [null, 128, null],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    // AXES --------------------------->
+
+    { // 3 planks + 2 sticks => 1 wooden axe
+        result: 135,
+        quantity: 1,
+        recipie: [
+            [
+                [8, 8, null],
+                [8, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, 8, 8],
+                [null, 128, 8],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    { // 3 stone + 2 sticks => 1 stone axe
+        result: 136,
+        quantity: 1,
+        recipie: [
+            [
+                [3, 3, null],
+                [3, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, 3, 3],
+                [null, 128, 3],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    { // 3 copper + 2 copper => 1 copper axe
+        result: 137,
+        quantity: 1,
+        recipie: [
+            [
+                [27, 27, null],
+                [27, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, 27, 27],
+                [null, 128, 27],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    { // 3 gold + 2 sticks => 1 golden axe
+        result: 138,
+        quantity: 1,
+        recipie: [
+            [
+                [29, 29, null],
+                [29, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, 29, 29],
+                [null, 128, 29],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    { // 3 iron + 2 sticks => 1 iron axe
+        result: 139,
+        quantity: 1,
+        recipie: [
+            [
+                [31, 31, null],
+                [31, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, 31, 31],
+                [null, 128, 31],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    { // 3 diamonds + 2 sticks => 1 diamond axe
+        result: 140,
+        quantity: 1,
+        recipie: [
+            [
+                [33, 33, null],
+                [33, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, 33, 33],
+                [null, 128, 33],
+                [null, 128, null]
+            ]
+        ]
+    },
+
+    // SHOVELS --------------------------->
+
+    { // 1 planks + 2 sticks => 1 wooden shovel
+        result: 141,
+        quantity: 1,
+        recipie: [
+            [
+                [8, null, null],
+                [128, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 8, null],
+                [null, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 8],
+                [null, null, 128],
+                [null, null, 128]
+            ]
+        ]
+    },
+
+    { // 1 stone + 2 sticks => 1 stone shovel
+        result: 142,
+        quantity: 1,
+        recipie: [
+            [
+                [3, null, null],
+                [128, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 3, null],
+                [null, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 3],
+                [null, null, 128],
+                [null, null, 128]
+            ]
+        ]
+    },
+
+    { // 1 copper + 2 copper => 1 copper shovel
+        result: 143,
+        quantity: 1,
+        recipie: [
+            [
+                [27, null, null],
+                [128, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 27, null],
+                [null, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 27],
+                [null, null, 128],
+                [null, null, 128]
+            ]
+        ]
+    },
+
+    { // 1 gold + 2 sticks => 1 golden shovel
+        result: 144,
+        quantity: 1,
+        recipie: [
+            [
+                [29, null, null],
+                [128, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 29, null],
+                [null, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 29],
+                [null, null, 128],
+                [null, null, 128]
+            ]
+        ]
+    },
+
+    { // 1 iron + 2 sticks => 1 iron shovel
+        result: 145,
+        quantity: 1,
+        recipie: [
+            [
+                [31, null, null],
+                [128, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 31, null],
+                [null, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 31],
+                [null, null, 128],
+                [null, null, 128]
+            ]
+        ]
+    },
+
+    { // 1 diamonds + 2 sticks => 1 diamond shovel
+        result: 146,
+        quantity: 1,
+        recipie: [
+            [
+                [33, null, null],
+                [128, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 33, null],
+                [null, 128, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 33],
+                [null, null, 128],
+                [null, null, 128]
+            ]
+        ]
+    },
+
+    // SWORDS --------------------------->
+
+    { // 2 planks + 1 sticks => 1 wooden sword
+        result: 147,
+        quantity: 1,
+        recipie: [
+            [
+                [8, null, null],
+                [8, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 8, null],
+                [null, 8, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 8],
+                [null, null, 8],
+                [null, null, 128]
+            ]
+        ]
+    },
+
+    { // 2 stone + 1 sticks => 1 stone sword
+        result: 148,
+        quantity: 1,
+        recipie: [
+            [
+                [3, null, null],
+                [3, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 3, null],
+                [null, 3, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 3],
+                [null, null, 3],
+                [null, null, 128]
+            ]
+        ]
+    },
+
+    { // 2 copper + 1 copper => 1 copper sword
+        result: 149,
+        quantity: 1,
+        recipie: [
+            [
+                [27, null, null],
+                [27, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 27, null],
+                [null, 27, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 27],
+                [null, null, 27],
+                [null, null, 128]
+            ]
+        ]
+    },
+
+    { // 2 gold + 1 sticks => 1 golden sword
+        result: 150,
+        quantity: 1,
+        recipie: [
+            [
+                [29, null, null],
+                [29, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 29, null],
+                [null, 29, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 29],
+                [null, null, 29],
+                [null, null, 128]
+            ]
+        ]
+    },
+
+    { // 2 iron + 1 sticks => 1 iron sword
+        result: 151,
+        quantity: 1,
+        recipie: [
+            [
+                [31, null, null],
+                [31, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 31, null],
+                [null, 31, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 31],
+                [null, null, 31],
+                [null, null, 128]
+            ]
+        ]
+    },
+
+    { // 2 diamonds + 1 sticks => 1 diamond sword
+        result: 152,
+        quantity: 1,
+        recipie: [
+            [
+                [33, null, null],
+                [33, null, null],
+                [128, null, null]
+            ],
+            [
+                [null, 33, null],
+                [null, 33, null],
+                [null, 128, null]
+            ],
+            [
+                [null, null, 33],
+                [null, null, 33],
+                [null, null, 128]
+            ]
+        ]
+    },
 ];
 
 export { recipies };
