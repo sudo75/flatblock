1991f45a3a52b54d991651131ae93327911659fe
Eric Skaftason <skaftasoneric@gmail.com>
2025-08-27 20:58:16 -0400

    added item bobbing animation

--- entities/entity_items.js
+++ entities/entity_items.js
@@ -21,10 +21,43 @@ class Entity_item extends Entity {
 
         this.entityType = 'item';
         this.key_input = [];
+
+        this.yOffset = 0;
+        this.boundToBlock = false;
     }
 
     update(input, deltaTime) {
-        super.update(input, deltaTime);
+
+        if (this.isOnSolidBlock()) {
+            this.boundToBlock = true;
+        }
+
+        if (this.isAboveSolidBlock() && this.boundToBlock) {
+            // Compute bobbing
+            const unix_time_ms = Date.now();
+            const period = 1000;
+            const amp = 0.1;
+
+            const yMovement = amp * Math.sin(((2 * Math.PI * unix_time_ms) / period)) + amp;
+
+            this.y += (yMovement - this.yOffset);
+            this.yOffset = yMovement;
+        } else {
+            super.update(input, deltaTime);
+            this.boundToBlock = false;
+        }
+    }
+
+    isAboveSolidBlock() { // solid block below, but not necessarily touching the item
+        if (this.y - 1 >= 0) {
+            for (let x = Math.floor(this.x); x <= this.calc.hardRoundDown(this.x + this.width_blocks); x++) {
+                if ((this.calc.isSolidBlock(x, Math.floor(this.y) - 1) && this.calc.hasPhysics(x, Math.floor(this.y) - 1))) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
     }
 
     run_gametick_logic(tick) {
